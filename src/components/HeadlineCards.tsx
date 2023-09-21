388ea1769b9ea702c1100631e9ef0dbfc9b8b574
Hanife Erkalan <73136159+eliferkln@users.noreply.github.com>
2023-09-21 22:31:07 +0300

    fix: style headline

--- src/components/HeadlineCards.tsx
+++ src/components/HeadlineCards.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeadlineCards = () => {
   return (
-    <div className="max-w-[1440px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+    <div className="max-w-[1440px] mx-auto p-4 pt-24 pb-12 grid md:grid-cols-3 gap-6">
       <div className="rounded-xl relative">
         <img
           className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
